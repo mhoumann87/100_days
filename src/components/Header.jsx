f975a42db93261e63a23472f267d800565b2eee1
Michael Houmann <mhoumann87@gmail.com>
2023-04-12 16:49:17 +0200

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <container className='container flex'>
+        <h1>100 days of code</h1>
+        <p>Progress</p>
+      </container>
+    </header>
+  );
+};
+export default Header;
